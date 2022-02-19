78e8aada9c26b67903eba1f2d2e983d4f615eebb
Aditya siripuram <adisiripuram007@gmail.com>
2022-02-19 22:14:06 +0530

    Assignment-1 Done!

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,14 +1,34 @@
+const readline= require("readline")
+
 function getNameFromCommandLine() {
     // Write you code here, name should be taken as args in process.argv
+    const name=process.argv[process.argv.length-1];
+    return name
 }
 
 function getNameFromEnv() {
     // Write your code here
+    var name2=process.env.name;
+    return name2
 }
 
 function getNameFromReadLine() {
     // Write your code here
+    const read = readline.createInterface({
+        input:process.stdin,
+        output:process.stdout
+    })
+
+    var Names = ""
+    read.question("",name=> {
+        Names = name
+        read.close()    
+    })
+    return Names
+
+
 }
+
 
 module.exports = {
     getNameFromCommandLine,
