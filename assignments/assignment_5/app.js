b2f842f8fb3d2bbb92b46009763458fd7bd5913b
Aditya siripuram <adisiripuram007@gmail.com>
2022-03-10 18:31:41 +0530

    Rest ApI

---
+++ assignments/assignment_5/app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require('mongoose');
+var jwt = require('jsonwebtoken');
+
+const loginRoutes = require("./routes/login");
+const postRoutes = require("./routes/posts");
+const userRoutes=require("./routes/posts");
+const SECRET = "RESTAPI";
+
+const app = express();
+app.set('views', "./views")
+app.set('view engine', "ejs")
+mongoose.connect('mongodb://localhost:27017/assignment_5');
+
+
+app.use("/posts", (req, res, next) =>{
+    var token = req.headers.authorization.split("alpha ")[1];
+
+    if(!token){
+        return res.status(401).json({
+            status: "failed",
+            message: "Token is missing"
+        })
+    }
+    // verify the token
+    jwt.verify(token, SECRET, async function(err, decoded) {
+        if(err){
+            return res.status(401).json({
+                status:"failed",
+                message: "Invalid token"
+            })
+        }
+        req.user = decoded.data;
+        next();
+    });
+});
+
+app.use("/", loginRoutes);
+app.use("/", postRoutes);
+app.use('/users',userRoutes)
+
+
+app.listen(5000, () => console.log("server is listening"));
